9f2b9ae53c1c42413f36efb82c1b4582e06ed746
Aitor Barrios Castejón <Aitor.Barrios@edu.uah.es>
2025-02-12 19:27:43 +0100

    Añadido Ejercicio 2.5 y 2.6 al repositorio Movido el Ejercicio 2.7 a la carpeta de la practica 1b Añadido la práctica EL1a

---
+++ EL1b/src/TestMain5.java
@@ -0,0 +1,28 @@
+import es.uah.matcomp.mp.e1.ejerciciosclases.Account;
+import es.uah.matcomp.mp.e1.ejerciciosclases.Customer;
+
+public class TestMain5 {
+    public static void main(String[] args) {
+        Customer c1 = new Customer(1, "Paquito", 'f');
+        Customer c2 = new Customer(2, "Emilio", 'm');
+        System.out.println(c1.getId());
+        System.out.println(c1.getName());
+        System.out.println(c1.getGender());
+        System.out.println(c1.toString());
+
+        Account a1 = new Account(1, c1, 1690.0);
+        Account a2 = new Account(2, c2);
+        System.out.println(a1.getBalance());
+        System.out.println(a1.getId());
+        System.out.println(a1.getCustomer());
+        System.out.println(a1.getCustomerName());
+        System.out.println(a1.toString());
+        a2.setBalance(769.0);
+        System.out.println(a2.toString());
+        a1.deposit(100.0);
+        System.out.println(a1.toString());
+        a1.withdraw(99.0);
+        System.out.println(a1.toString());
+        a2.withdraw(1000.0);
+    }
+}
