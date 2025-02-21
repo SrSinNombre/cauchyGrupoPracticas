4b4a31f5d5af11d59148d1ca1ca54f2c40d35d3e
Faateh Guliyev <faateh.guliyev@edu.uah.es>
2025-02-21 08:50:40 +0100

    Lista basica esquibidi

--- estructuras de datos/src/ListaBasica.java
+++ estructuras de datos/src/ListaBasica.java
@@ -1,5 +1,34 @@
 public class ListaBasica<T> {
-    private int T_max;
-    private Object[] array;
 
+    private T[] array;
+    private int T_Max;
+
+    public ListaBasica(int capacidad) {
+        this.T_Max = capacidad;
+        this.array = (T[]) new Object[T_Max];
+    }
+
+
+    public void setElemento(int indice, T e) {
+        if (indice >= 0 && indice < T_Max) {
+            array[indice] = e;
+        } else {
+            System.out.println("Índice fuera de rango.");
+        }
+    }
+
+    public T getElemento(int indice) {
+        if (indice >= 0 && indice < T_Max) {
+            return array[indice];
+        }
+        return null;
+    }
+
+    public void mostrarElementos() {
+        for (T e : array) {
+            if (e != null) {
+                System.out.println(e.toString());
+            }
+        }
+    }
 }
