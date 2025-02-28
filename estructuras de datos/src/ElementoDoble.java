7008417b3bd28ffc6952dd36edebd4581479154b
Faateh Guliyev <faateh.guliyev@edu.uah.es>
2025-02-28 08:43:11 +0100

    torrejón > azuqueca

---
+++ estructuras de datos/src/ElementoDoble.java
@@ -0,0 +1,20 @@
+public class ElementoDoble<T> {
+    private T dato;
+    private Elemento<T> anterior;
+    private Elemento<T> siguiente;
+
+    public ElementoDoble(T dato){
+        this.dato = dato;
+        anterior = null;
+        siguiente = null;
+    }
+    public T getDato(){
+        return dato;
+    }
+    public Elemento<T> getSiguiente(){
+        return siguiente;
+    }
+    public Elemento<T> getAnterior(){
+        return anterior;
+    }
+}
