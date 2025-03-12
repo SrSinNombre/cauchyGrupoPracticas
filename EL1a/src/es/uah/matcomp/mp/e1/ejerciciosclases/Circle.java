e2aedb3661eca31fca04e92abc69ac7bbbcd4337
Aitor Barrios Castejón <Aitor.Barrios@edu.uah.es>
2025-03-12 08:23:29 +0100

    Porque hay que rellenar la perra mamada esta

--- EL1a/src/es/uah/matcomp/mp/e1/ejerciciosclases/Circle.java
+++ EL1a/src/es/uah/matcomp/mp/e1/ejerciciosclases/Circle.java
@@ -1,27 +1,45 @@
 package es.uah.matcomp.mp.e1.ejerciciosclases;
 
 public class Circle {
-    private double radius = 1.0;
+    private double radius;
+    private String color;
 
-    public Circle() {
+    public Circle(){
         radius = 1.0;
+        color = "red";
     }
+
     public Circle(double radius) {
         this.radius = radius;
+        color = "red";
     }
-    public double getRadius() {
+
+    public Circle(double radius, String color){
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius(){
         return radius;
     }
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-    public double getArea() {
+
+    public double getArea(){
         return Math.PI * radius * radius;
     }
-    public double getCircumference() {
-        return 2 * Math.PI * radius;
+
+    public String getColor(){
+        return color;
     }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
     public String toString() {
-        return "Circle[radius=" + radius + "]";
+        return "Circle [radius=" + radius + ", color=" + color + "]";
     }
 }
